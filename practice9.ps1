07c39e1afd1f978d3472bdc509911b20f546dc97
Markkus Hipponen <markkus.hipponen@voco.ee>
2025-04-30 11:59:27 +0300

    count statement sum notepad skript

---
+++ practice9.ps1
@@ -0,0 +1,14 @@
+
+# do while loop kontrollimaks kas notepad tootab
+
+do {
+    if (Get-Process notepad -ErrorAction SilentlyContinue) {
+        Write-Host "Notepad is running"
+        $count += 1
+    }
+    Start-Sleep -Seconds 1
+} while (Get-Process notepad -ErrorAction SilentlyContinue)
+
+# Teavitame kasutajat, et yhtegi notepadi enam ei toota
+
+Write-Host "Teavitati $count korda"
